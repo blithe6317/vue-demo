ad0e904e963e779a4c800805fa2b773d625f7156
blithe6317 <1160457037@qq.com>
2022-01-16 22:20:31 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <h2>{{ count }}</h2>
+    <button @click="add">点我</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    add() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.app {
+  button {
+    background-color: red;
+  }
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "app.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["vue-style-loader", "css-loader", "less-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+  ],
+};
